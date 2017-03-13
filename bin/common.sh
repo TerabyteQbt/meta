f917db16eb7cbc30cad7bd862fce61b5bf519a86
Carl Myers <cmyers@cloudera.com>
2017-03-13 16:03:17 -0700

    WIP: deploy-to-github and wrapper install script

---
+++ bin/common.sh
@@ -0,0 +1,9 @@
+# Change these to update qbt version.
+
+export META_TOOLS_COMMIT="35b0ae06721e396a1ece243fa4ff94f47ea8fdcd" # the sha1 we built this from, FYI
+export META_TOOLS_CV="e8605d60e705a93c653307c425829047068ee4b1" # the CV that results (multiple commits might generate the same CV)
+export META_TOOLS_SHA256="3ed6fa685a09b981a1b8d85d7a650888c054ead50f1466399da153da9fac46f7" # this is the sha256sum of the build published - we are not bit-for-bit reproducible, so this could change if you build your own.
+
+# github is as good a hosting platform as any
+export PUBLIC_QBT_URL="https://github.com/TerabyteQbt/meta/releases/download/metatools-1489427073/meta_tools-e8605d60e705a93c653307c425829047068ee4b1.tar.gz"
+
